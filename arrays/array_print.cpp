44c9cfebbe110ae9f1ca37709cb60c6a13708bbf
Hemanth-KumarR <hemanthhemanth65435@gmail.com>
2026-02-16 22:37:59 +0530

    array print

--- arrays/array_print.cpp
+++ arrays/array_print.cpp
@@ -2,16 +2,12 @@
 using namespace std;
 int main()
 {
-   int arr[10];
-   cout<<"Enter 10 elements of the array: ";
-   for(int i = 0; i < 10; i++)
-   {
-       cin>>arr[i];
-   }
-   cout<<"Elements of the array are: ";
-   for(int i = 0; i < 10; i++)
-   {
-       cout<<arr[i]<<" ";
-   }
-   return 0;
+   
+    int arr[5] = {1, 2, 3, 4, 5};
+    for(int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
