76093d01a6c07a686a64cc31cb40e8e9a5ef872c
Hemanth-KumarR <hemanthhemanth65435@gmail.com>
2026-02-24 22:59:44 +0530

    Insertion sort program

---
+++ arrays/insertion_sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+
+    cout << "Enter 5 elements:" << endl;
+
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Insertion Sort
+    for(int i = 1; i < 5; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    cout << "Sorted array:" << endl;
+
+    for(int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
